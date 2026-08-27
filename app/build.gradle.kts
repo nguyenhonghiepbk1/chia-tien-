5249f8a60cabcc8e44fd8c9fb2de667ab4554453
nguyenhonghiepbk1 <nguyenhonghiepbk1@gmail.com>
2026-08-27 15:29:38 +0700

    chore: bump version to 12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.tripfinance.vszupq"
     minSdk = 24
     targetSdk = 36
-    versionCode = 11
-    versionName = "11.0"
+    versionCode = 12
+    versionName = "12.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
